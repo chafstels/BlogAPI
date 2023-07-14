8728475fab6f6b677fa052981af461e7aede266d
Anton <anton@Anton-MacBook-Pro.local>
2023-07-14 15:01:24 +0600

    add todo

--- todo_app/serializers.py
+++ todo_app/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 from .models import Category, Todo
+
+
 class CategorysSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+
 class TodoSerializer(serializers.ModelSerializer):
-    # category = CategorySerializer()
+    owner_username = serializers.ReadOnlyField(source='owner.username')
+    category_name = serializers.ReadOnlyField(source='category.name')
     class Meta:
         model = Todo
         fields = '__all__'
